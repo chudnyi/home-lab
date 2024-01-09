ec83ba0fb52b2d66967b69523ba0bb06b7afd31f
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2024-01-09 09:31:47 +0300

    Миграция конфигурауии домашнего сервера с ручного создания сервисов в Portainer на единый стек, создаваемый через SSH (DOCKER_HOST)

---
+++ README.md
@@ -0,0 +1,4 @@
+## Запуск docker compose стека не из под рута
+
+Нужно добавить пользователя в группу docker: https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user.
+
---
+++ cmd.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#set -x
+
+# https://www.docker.com/blog/how-to-deploy-on-remote-docker-hosts-with-docker-compose/
+
+[[ ! -f ".env" ]] && echo "Не найден конфигурационный файл: .env" && exit 1
+source .env
+# Команды docker compose будут выполняться на этом хосте
+export DOCKER_HOST="ssh://$SERVER_USER@$SERVER_HOST"
+
+# Чтобы убрать сервис с сервера, закомментируй его конфиг в списке ниже
+COMPOSE_STACKS=(
+  "-f" "compose/traefik/docker-compose.yml"
+  "-f" "compose/docker-host/docker-compose.yml"
+  "-f" "compose/portainer/docker-compose.yml"
+  "-f" "compose/jellyfin/docker-compose.yml"
+  "-f" "compose/ssh-tunnel/docker-compose.yml"
+  "-f" "compose/freshrss/docker-compose.yml"
+  "-f" "compose/syncthing/docker-compose.yml"
+  "-f" "compose/transmission/docker-compose.yml"
+  "-f" "compose/uptime-kuma/docker-compose.yml"
+  "-f" "compose/adguard/docker-compose.yml"
+#  "-f" "compose/redis/docker-compose.yml"
+#  "-f" "compose/photoprism/docker-compose.yml"
+)
+
+
+up() {
+  echo "DOCKER_HOST: $DOCKER_HOST"
+  docker compose -p "${DOCKER_STACK_NAME}" --env-file .env \
+    "${COMPOSE_STACKS[@]}" \
+    up --detach --remove-orphans
+}
+
+"$@"
